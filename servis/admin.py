ad7b24040fa5859fe8c5cd3ead8128775faa70a6
Arthur Fakeev <RikyBone6@gmail.com>
2022-07-14 17:14:21 +0400

    Lesson Models

--- servis/admin.py
+++ servis/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-from .models import Product, Bin
+from .models import Product, Bin, Home
 
 admin.site.register(Bin)
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'cost']
+    list_display = ['id', 'title', 'cost', 'isActive']
     fields = ['id', 'title']
     list_filter = ['cost']
     search_fields = ['title']
 
+
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ['size', 'cost', 'adr', 'bol']
+    # fields = ['id', 'title']
+    # list_filter = ['cost']
+    # search_fields = ['title']
 #admin.site.register(Product, ProductAdmin)
--- servis/models.py
+++ servis/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 class Product(models.Model):
@@ -5,6 +6,7 @@ class Product(models.Model):
     id = models.IntegerField(primary_key=True)
     cost = models.CharField(max_length=200, null=True)
     title = models.CharField(max_length=200, null=True)
+    isActive = models.BooleanField(verbose_name='Active', default=True, null=True)
 
     def __str__(self):
         return self.title
@@ -18,3 +20,13 @@ class Bin(models.Model):
 
     def __str__(self):
         return self.title
+
+class Home(models.Model):
+
+    size = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="The size", null=True)
+    cost = models.IntegerField(verbose_name='Price', null=True)
+    adr = models.CharField(max_length=200, verbose_name='Addres', default='Armavits Str')
+    bol = models.BooleanField(verbose_name="balcony", default=True)
+
+    def __str__(self):
+        return self.adr
