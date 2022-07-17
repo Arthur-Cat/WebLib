2906e5f9c8a94ecb50c0f587c5222801b5faf3ee
Arthur Fakeev <RikyBone6@gmail.com>
2022-07-17 14:43:01 +0400

    HTML

--- webLib/urls.py
+++ webLib/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from webLib.views import main, authors, books, about
+from webLib.views import main, authors, books, about, author_id, book_title
 
-urlpatterns = {
+urlpatterns = [
     path('', main, name='webLib'),
     path('authors', authors, name='authors'),
+    path('authors/<uuid:pk>', author_id, name='author_id'),     
+    path('book/<str:title_1>', book_title, name='book_title'),    
     path('books', books, name='books'),
     path('about', about, name='about'),
-}
+]
--- webLib/views.py
+++ webLib/views.py
@@ -8,9 +8,21 @@ def authors(request):
     all_authors = {'authors': Author.objects.all()}
     return render(request, 'webLib/authors.html', all_authors)
 
-def books(request):
+def author_id(request, pk):
+    author = Author.objects.get(pk=pk)
+    books_amout = author.book_set.count()
+    found_author = {'author': author, 'books_amout': books_amout}
+    return render(request, 'webLib/author_id.html', found_author)
+
+def books(request):    
     all_books = {'books': Book.objects.all()}
     return render(request, 'webLib/books.html', all_books)
 
+def book_title(request, title_1):
+    book = Book.objects.get(title_1=title_1)
+    all_books = {'book': book}
+    return render(request, 'webLib/book_title.html', all_books)
+
 def about(request):
     return render(request, 'webLib/about.html')
+
